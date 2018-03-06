ddb1ee216d255eb6287f846af5580e7b80a496c5
Frédéric Noël Conseil <contact@fredericnoel.com>
2018-03-06 12:34:03 +0100

    Inclusion automatique avec vérification par double extension.

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <?php
 include "./includes/header.php";
 
-echo "<h1>Ceci est le corps de mon index.php</h1>";
+$page = (isset($_GET['page']) && $_GET['page'] != "") ? $_GET['page'] : "accueil";
+$page = "./includes/" . $page . ".inc.php";
 
+$files = glob("./includes/*.inc.php");
 
+if (in_array($page, $files)) {
+    include $page;
+}
+
+else {
+    include "./includes/accueil.inc.php";
+}
 
 include "./includes/footer.php";
