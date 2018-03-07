aa2b3800576b14a8c2859ed29920319a92ae1d9a
Frédéric Noël Conseil <contact@fredericnoel.com>
2018-03-07 09:49:08 +0100

    Ecrire formulaire inscription

---
+++ includes/frmInscription.php
@@ -0,0 +1,23 @@
+<form method="post" action="#">
+    <div>
+        <label for="nom">Nom : </label>
+        <input type="text" name="nom" />
+    </div>
+    <div>
+        <label for="prenom">Prénom : </label>
+        <input type="text" name="prenom" />
+    </div>
+    <div>
+        <label for="mail">Mail : </label>
+        <input type="text" name="mail" />
+    </div>
+    <div>
+        <label for="mdp">Mot de passe : </label>
+        <input type="password" name="mdp" />
+    </div>
+    <div>
+        <input type="reset" value="Effacer" />
+        <input type="submit" value="Envoyer" />
+        <input type="hidden" name="frmInscription" />
+    </div>
+</form>
