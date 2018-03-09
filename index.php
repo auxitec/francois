398ce132e5e97bb91da3cd622a31bbfffec42773
Frédéric Noël Conseil <contact@fredericnoel.com>
2018-03-09 16:24:47 +0100

    Exemple @ devant fonction. Ajout de va_dump() sur la superglobale de session pour en vérifier le contenu.

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+@session_start();
+var_dump($_SESSION);
 ini_set("smtp_port", 1025);
 include "./functions/classAutoLoader.php";
 spl_autoload_register('classAutoLoader');
@@ -9,6 +11,7 @@ spl_autoload_register('classAutoLoader');
     <meta charset="utf-8" />
     <title>Auxitec Blog</title>
     <link rel="stylesheet" href="./assets/css/style.css" />
+    <script type="text/javascript" src="./assets/javascript/redirection.js"></script>
 </head>
 <body>
 <div id="container">
