29514d6ffa98b2a1f9b8dde2e019c928a9160388
Frédéric Noël Conseil <contact@fredericnoel.com>
2018-03-09 11:46:53 +0100

    Ecriture formulaire login

---
+++ includes/frmLogin.php
@@ -0,0 +1,14 @@
+<form method="post" action="#">
+        <label for="mail">Mail : </label>
+        <input type="text" name="mail" />
+    </div>
+    <div>
+        <label for="mdp">Mot de passe : </label>
+        <input type="password" name="mdp" />
+    </div>
+    <div>
+        <input type="reset" value="Effacer" />
+        <input type="submit" value="Envoyer" />
+        <input type="hidden" name="frmLogin" />
+    </div>
+</form>
